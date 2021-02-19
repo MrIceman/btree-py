c374474cc5bfa22d83ceb06f698523c46e00cde8
Martin Nowosad <mrtnnwsd@gmail.com>
2021-02-19 01:39:50 +0100

    updated main.py for demosntration purposes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-def fibonacci(n):
-    if n == 0 or n == 1:
-        return 1
-    a = fibonacci(n - 2)
-    b = fibonacci(n - 1)
-    return a + b
+from tree import BTree
 
+# k defines the key capacity for each node. Note that the actual
+# key capacity is k-1
+k = 10195
+# the amount of nodes you wanna enter
+n = 450005
+tree = BTree(k)
+for i in range(0, n):
+    tree.insert(i, None)
+    if i % 100000 == 0:
+        print(f'{i} inserted')
 
-for i in range(0, 10):
-    print(f'{i}: {fibonacci(i)}')
+print(f"root after execution. Height: {tree.height}")
+
+for i in range(0, n):
+    result = tree.search(i)
+    if i not in result.keys:
+        # this code will never run :-) At least it shouldn't...
+        raise Exception(f"{i} not contained. The tree is broken :(")
